f642eb12878c712339e09c664474c8157857d44a
Andrés Felipe Rodríguez Gallego <79536152+andres-rod-g@users.noreply.github.com>
2024-11-21 03:19:08 -0500

    2D Models done

--- src/components/graphRender/graphRenderer.component.tsx
+++ src/components/graphRender/graphRenderer.component.tsx
@@ -1,4 +1,4 @@
-import { Circle, Coordinates, Mafs, useMovablePoint, vec } from "mafs";
+import { Circle, Coordinates, Ellipse, Mafs, Polygon, Theme, Transform, useMovablePoint, vec } from "mafs";
 
 interface CustomProps {
     id: string;
@@ -13,12 +13,61 @@ const graphRenderer: React.FC<CustomProps> = ({
     const renderer = () => {
         switch (id) {
             case "triangulo":
-                return <Mafs viewBox={{ y: [-2, 2] }}>
-                    <Coordinates.Cartesian />
-                    <Circle center={[0, 0]} radius={object.radius} />
+                return <Mafs viewBox={{ y: [-(object.base * 1.5), object.altura * 1.5] }}>
+                    <Coordinates.Cartesian
+                        xAxis={{
+                            lines: 1,
+                            labels: (object.altura > 9 || object.base > 9 ? (n) => (isOdd(n) ? n : "") : (n) => n),
+                        }}
+                        yAxis={{
+                            lines: 1,
+                            labels: (object.altura > 9 || object.base > 9 ? (n) => (isOdd(n) ? n : "") : (n) => n),
+                        }}
+                    />
+                    <Polygon
+                        points={[[0, object.altura / 2], [object.base / 2, -object.altura / 2], [-object.base / 2, -object.altura / 2]]}
+                        color={Theme.blue}
+                    />
                 </Mafs>;
             case "elipse":
-                return <></>
+                return (
+                    <Mafs viewBox={{ y: [-(object.a * 1.5), object.b * 1.5] }} >
+                        <Coordinates.Cartesian subdivisions={0.25}
+                            xAxis={{
+                                lines: 1,
+                                labels: (object.a > 9 || object.b > 9 ? (n) => (isOdd(n) ? n : "") : (n) => n),
+                            }}
+                            yAxis={{
+                                lines: 1,
+                                labels: (object.a > 9 || object.b > 9 ? (n) => (isOdd(n) ? n : "") : (n) => n),
+                            }} />
+                        <Ellipse
+                            center={[0, 0]}
+                            radius={[Math.abs(object.a), Math.abs(object.b)]}
+                        />
+
+                        <Transform translate={[0, 0]}>
+                            <Transform rotate={0}>
+                                {/*
+                             * Display a little hint that the
+                             * point is meant to move radially
+                             */}
+                                {/* <Circle
+                                    center={[0, 0]}
+                                    radius={hintRadius}
+                                    strokeStyle="dashed"
+                                    strokeOpacity={0.3}
+                                    fillOpacity={0}
+                                /> */}
+
+                                <Ellipse
+                                    center={[0, 0]}
+                                    radius={[Math.abs(object.a), Math.abs(object.b)]}
+                                />
+                            </Transform>
+                        </Transform>
+                    </Mafs>
+                );
             case "circulo":
                 return (
                     <Mafs viewBox={{ y: [-(object.radio * 1.5), object.radio * 1.5] }} >
@@ -35,22 +84,44 @@ const graphRenderer: React.FC<CustomProps> = ({
                     </Mafs>
                 );
             case "rectangulo":
-                return <></>
+                return (
+                    <Mafs viewBox={{ y: [-(object.base * 1.5), object.altura * 1.5] }}>
+                        <Coordinates.Cartesian
+                            xAxis={{
+                                lines: 1,
+                                labels: (object.altura > 9 || object.base > 9 ? (n) => (isOdd(n) ? n : "") : (n) => n),
+                            }}
+                            yAxis={{
+                                lines: 1,
+                                labels: (object.altura > 9 || object.base > 9 ? (n) => (isOdd(n) ? n : "") : (n) => n),
+                            }}
+                        />
+                        <Polygon
+                            points={[
+                                [-object.base/2, object.altura / 2], 
+                                [object.base/2, object.altura / 2], 
+                                [object.base / 2, -object.altura / 2], 
+                                [-object.base / 2, -object.altura / 2],
+                            ]}
+                            color={Theme.blue}
+                        />
+                    </Mafs>
+                );
             case "esfera":
-                return <></>
+return <></>
             case "cubo":
-                return <></>
+return <></>
             case "triangulo":
-                return <></>
+return <></>
             case "cono":
-                return <></>
+return <></>
         }
     }
 
-    return <>
-        {/* {id} */}
-        {renderer()}
-    </>
+return <>
+    {/* {id} */}
+    {renderer()}
+</>
 }
 
 function isOdd(n: number) {
